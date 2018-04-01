c816c2a662b411948f1c087f8e206cb165a7316c
entry@wa2c.com <entry@wa2c.com>
2018-04-01 17:02:01 +0900

    - Modified SiteActivity

--- app/src/main/java/com/wa2c/android/medoly/plugin/action/lyricsscraper/activity/SiteActivity.kt
+++ app/src/main/java/com/wa2c/android/medoly/plugin/action/lyricsscraper/activity/SiteActivity.kt
@@ -1,33 +1,17 @@
 package com.wa2c.android.medoly.plugin.action.lyricsscraper.activity
 
-import android.app.ActionBar
 import android.app.Activity
 import android.app.LoaderManager
 import android.content.Context
 import android.content.CursorLoader
 import android.content.Intent
 import android.content.Loader
-import android.content.SharedPreferences
 import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.text.TextUtils
-import android.view.KeyEvent
-import android.view.Menu
-import android.view.MenuItem
-import android.view.MotionEvent
-import android.view.View
-import android.view.ViewGroup
-import android.widget.AbsListView
-import android.widget.AdapterView
-import android.widget.CursorAdapter
-import android.widget.ImageButton
-import android.widget.ListView
-import android.widget.RadioButton
-import android.widget.TextView
-
-import com.wa2c.android.medoly.plugin.action.lyricsscraper.BuildConfig
+import android.view.*
+import android.widget.*
 import com.wa2c.android.medoly.plugin.action.lyricsscraper.R
 import com.wa2c.android.medoly.plugin.action.lyricsscraper.db.GroupColumn
 import com.wa2c.android.medoly.plugin.action.lyricsscraper.db.SiteColumn
@@ -36,26 +20,14 @@ import com.wa2c.android.medoly.plugin.action.lyricsscraper.service.SpreadSheetRe
 import com.wa2c.android.medoly.plugin.action.lyricsscraper.util.AppUtils
 import com.wa2c.android.medoly.plugin.action.lyricsscraper.util.Prefs
 import kotlinx.android.synthetic.main.activity_group.*
-
-import java.util.Locale
+import kotlinx.android.synthetic.main.layout_site_item.view.*
+import java.util.*
 
 
 class SiteActivity : Activity() {
 
-//    /** Preferences.  */
-//    private var preference: SharedPreferences? = null
-
     private lateinit var prefs: Prefs
-
-    /** Cursor Adapter.  */
     private lateinit var cursorAdapter: SheetCursorAdapter
-    /** Loader manager.  */
-//    private var loaderManager: LoaderManager? = null
-
-//    private var listLayout: ViewGroup? = null
-//
-//    private var loadingLayout: ViewGroup? = null
-
 
     /**
      * Cursor loader.
@@ -97,13 +69,9 @@ class SiteActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //this.preference = PreferenceManager.getDefaultSharedPreferences(this)
         prefs = Prefs(this)
 
-
         setContentView(R.layout.activity_group)
-//        listLayout = findViewById<View>(R.id.groupListView) as ViewGroup
-//        loadingLayout = findViewById<View>(R.id.loadingLayout) as ViewGroup
         groupListView.visibility = View.VISIBLE
         loadingLayout.visibility = View.INVISIBLE
 
@@ -119,12 +87,10 @@ class SiteActivity : Activity() {
         cursorAdapter = SheetCursorAdapter(this, CURSOR_TYPE_GROUP)
         val listView = findViewById<View>(R.id.groupListView) as ListView
         listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
-            val holder = view.tag as SheetCursorAdapter.ListViewHolder
             if (cursorAdapter.cursorType == CURSOR_TYPE_GROUP) {
-                openSiteList(holder.GroupId)
+                openSiteList(view.tag as String)
             } else if (cursorAdapter.cursorType == CURSOR_TYPE_SITE) {
-                //preference!!.edit().putString(getString(R.string.prefkey_selected_site_id), holder.SiteId).apply()
-                prefs.putValue(R.string.prefkey_selected_site_id, holder.SiteId)
+                prefs.putValue(R.string.prefkey_selected_site_id, view.tag as String)
                 cursorAdapter.notifyDataSetChanged()
             }
         }
@@ -132,7 +98,6 @@ class SiteActivity : Activity() {
         listView.adapter = cursorAdapter
 
         // create load manager
-//        loaderManager = this.getLoaderManager()
         loaderManager.restartLoader(CURSOR_LOADER_ID, bundle, cursorLoaderCallbacks)
     }
 
@@ -172,19 +137,6 @@ class SiteActivity : Activity() {
                     }
                 })
 
-//                task.setOnPropertyActionListener { isSucceeded ->
-//                    if (isSucceeded) {
-//                        openGroupList()
-//                        AppUtils.showToast(applicationContext, R.string.message_renew_list_succeeded)
-//                    } else {
-//                        AppUtils.showToast(applicationContext, R.string.message_renew_list_failed)
-//                    }
-//                    val bundle = Bundle()
-//                    bundle.putInt(CURSOR_TYPE, CURSOR_TYPE_GROUP)
-//                    loaderManager!!.restartLoader(CURSOR_LOADER_ID, bundle, cursorLoaderCallbacks)
-//                    groupListView!!.visibility = View.VISIBLE
-//                    loadingLayout!!.visibility = View.INVISIBLE
-//                }
                 groupListView.visibility = View.INVISIBLE
                 loadingLayout.visibility = View.VISIBLE
                 loaderManager.destroyLoader(CURSOR_LOADER_ID)
@@ -206,7 +158,7 @@ class SiteActivity : Activity() {
 
     override fun dispatchKeyEvent(e: KeyEvent): Boolean {
         when (e.keyCode) {
-        // back key
+            // back key
             KeyEvent.KEYCODE_BACK -> if (cursorAdapter.cursorType == CURSOR_TYPE_SITE) {
                 openGroupList()
                 return true
@@ -224,7 +176,7 @@ class SiteActivity : Activity() {
         val bundle = Bundle()
         bundle.putInt(CURSOR_TYPE, CURSOR_TYPE_SITE)
         bundle.putString(GROUP_ID, groupId)
-        loaderManager!!.restartLoader(CURSOR_LOADER_ID, bundle, cursorLoaderCallbacks)
+        loaderManager.restartLoader(CURSOR_LOADER_ID, bundle, cursorLoaderCallbacks)
     }
 
     /**
@@ -243,93 +195,59 @@ class SiteActivity : Activity() {
      */
     private class SheetCursorAdapter internal constructor(context: Activity, internal var cursorType: Int) : CursorAdapter(context, null, true) {
 
-//        private val preference: SharedPreferences
         private val prefs = Prefs(context)
-
         private val localeGroupNameCol: GroupColumn = GroupColumn.findLocaleColumn(Locale.getDefault())
 
-//        init {
-//            this.preference = PreferenceManager.getDefaultSharedPreferences(context)
-//            this.localeGroupNameCol = GroupColumn.findLocaleColumn(Locale.getDefault())
-//        }
-
         override fun newView(context: Context, cursor: Cursor, parent: ViewGroup): View {
-            val view = View.inflate(context, R.layout.layout_site_item, null)
-            val holder = ListViewHolder()
-            holder.SelectRadioButton = view.findViewById<View>(R.id.siteSelectRadioButton) as RadioButton
-            holder.TitleTextView = view.findViewById<View>(R.id.siteParamTitleTextView) as TextView
-            holder.UriTextView = view.findViewById<View>(R.id.siteParamUriTextView) as TextView
-            holder.LaunchImageButton = view.findViewById<View>(R.id.siteLaunchImageButton) as ImageButton
-            view.tag = holder
-            return view
+            return View.inflate(context, R.layout.layout_site_item, null)
         }
 
         override fun bindView(view: View, context: Context, cursor: Cursor) {
-            val holder = view.tag as ListViewHolder
             if (cursorType == CURSOR_TYPE_GROUP) {
-                holder.SelectRadioButton!!.visibility = View.GONE
-                holder.LaunchImageButton!!.visibility = View.GONE
+                view.siteSelectRadioButton.visibility = View.GONE
+                view.siteLaunchImageButton.visibility = View.GONE
 
                 val title = cursor.getString(cursor.getColumnIndexOrThrow(localeGroupNameCol.columnKey))
-                holder.GroupId = cursor.getString(cursor.getColumnIndexOrThrow(GroupColumn.GROUP_ID.columnKey))
-                holder.TitleTextView!!.text = title
-                holder.UriTextView!!.visibility = View.GONE
-
+                view.tag = cursor.getString(cursor.getColumnIndexOrThrow(GroupColumn.GROUP_ID.columnKey))
+                view.siteParamTitleTextView.text = title
+                view.siteParamUriTextView.visibility = View.GONE
             } else {
                 // ID
-                holder.GroupId = cursor.getString(cursor.getColumnIndexOrThrow(SiteColumn.GROUP_ID.columnKey))
-                holder.SiteId = cursor.getString(cursor.getColumnIndexOrThrow(SiteColumn.SITE_ID.columnKey))
+                val siteId = cursor.getString(cursor.getColumnIndexOrThrow(SiteColumn.SITE_ID.columnKey));
+                view.tag = siteId
 
                 // select
-                holder.SelectRadioButton!!.visibility = View.VISIBLE
-                //if (holder.SiteId == preference.getString(context.getString(R.string.prefkey_selected_site_id), "-1")) {
-                if (holder.SiteId == prefs.getString(R.string.prefkey_selected_site_id, "-1")) {
-                    holder.SelectRadioButton!!.isChecked = true
-                } else {
-                    holder.SelectRadioButton!!.isChecked = false
-                }
-                holder.SelectRadioButton!!.setOnClickListener {
-                    //preference.edit().putString(context.getString(R.string.prefkey_selected_site_id), holder.SiteId).apply()
-                    prefs.putValue(R.string.prefkey_selected_site_id, holder.SiteId)
+                view.siteSelectRadioButton!!.visibility = View.VISIBLE
+                view.siteSelectRadioButton!!.isChecked = (siteId == prefs.getString(R.string.prefkey_selected_site_id, "-1"))
+                view.siteSelectRadioButton!!.setOnClickListener {
+                    prefs.putValue(R.string.prefkey_selected_site_id, siteId)
                     notifyDataSetChanged()
                 }
 
                 // title
                 val title = cursor.getString(cursor.getColumnIndexOrThrow(SiteColumn.SITE_NAME.columnKey))
-                holder.TitleTextView!!.text = title
-                holder.TitleTextView!!.setOnTouchListener { _, event -> view.onTouchEvent(event) }
+                view.siteParamTitleTextView!!.text = title
+                view.siteParamTitleTextView!!.setOnTouchListener { _, event -> view.onTouchEvent(event) }
 
                 // uri
                 val uri = cursor.getString(cursor.getColumnIndexOrThrow(SiteColumn.SITE_URI.columnKey))
-                holder.UriTextView!!.visibility = View.VISIBLE
-                holder.UriTextView!!.text = uri
-                holder.LaunchImageButton!!.tag = uri
-                holder.LaunchImageButton!!.visibility = View.VISIBLE
+                view.siteParamUriTextView!!.visibility = View.VISIBLE
+                view.siteParamUriTextView!!.text = uri
+                view.siteLaunchImageButton!!.tag = uri
+                view.siteLaunchImageButton!!.visibility = View.VISIBLE
                 if (!TextUtils.isEmpty(uri)) {
-                    holder.LaunchImageButton!!.isEnabled = true
-                    holder.LaunchImageButton!!.setOnClickListener { v ->
+                    view.siteLaunchImageButton!!.isEnabled = true
+                    view.siteLaunchImageButton!!.setOnClickListener { v ->
                         val sheetUri = Uri.parse(v.tag as String)
                         val browserIntent = Intent(Intent.ACTION_VIEW, sheetUri)
                         context.startActivity(browserIntent)
                     }
                 } else {
-                    holder.LaunchImageButton!!.isEnabled = false
+                    view.siteLaunchImageButton!!.isEnabled = false
                 }
             }
         }
 
-        /**
-         * ViewHolder for QueueListView.
-         */
-        class ListViewHolder {
-            internal var GroupId: String? = null
-            internal var SiteId: String? = null
-
-            internal var SelectRadioButton: RadioButton? = null
-            internal var TitleTextView: TextView? = null
-            internal var UriTextView: TextView? = null
-            internal var LaunchImageButton: ImageButton? = null
-        }
     }
 
     companion object {
